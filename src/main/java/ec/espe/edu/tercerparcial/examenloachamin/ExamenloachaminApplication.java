e73d9a9e112e6039ab9968896753499a6f04cde5
GisseTefy <gisselle.estefania1997@gmail.com>
2024-03-11 19:31:22 -0500

    Proyecto productos creado

---
+++ src/main/java/ec/espe/edu/tercerparcial/examenloachamin/ExamenloachaminApplication.java
@@ -0,0 +1,13 @@
+package ec.espe.edu.tercerparcial.examenloachamin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenloachaminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenloachaminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ec/espe/edu/tercerparcial/examenloachamin/ExamenloachaminApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.espe.edu.tercerparcial.examenloachamin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenloachaminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
